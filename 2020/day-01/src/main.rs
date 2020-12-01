f9162e3f2e41c48a95f1092911d7beaf47b4891f
Dan Stoza <dstoza@gmail.com>
2020-12-01 09:30:40 -0800

    Add more parsing

--- 2020/day-01/src/main.rs
+++ 2020/day-01/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs::File, io::BufReader};
+use std::{env, fs::File, io::{BufRead, BufReader}};
 
 fn sum_product(array: Vec<i32>) -> i32 {
     0
@@ -11,9 +11,27 @@ fn main() {
     }
 
     let filename = &args[0];
-    let mut file = File::open(filename).expect(format!("Failed to open file {}", filename).as_str());
+    let file = File::open(filename).expect(format!("Failed to open file {}", filename).as_str());
     let mut reader = BufReader::new(file);
 
     let mut line = String::new();
-    let array = Vec::<i32>::new();
+    let mut array = Vec::<i32>::new();
+    loop {
+        let bytes = reader.read_line(&mut line).expect("Failed to read line");
+        if bytes == 0 {
+            println!("bytes 0");
+            break;
+        }
+
+        println!("Parsing {}", &line);
+
+        if let Ok(integer) = line.as_str().parse::<i32>() {
+            array.push(integer);
+        }
+    }
+
+    for i in &array {
+        println!("{}", i);
+    }
+    println!("Result: {}", sum_product(array));
 }
