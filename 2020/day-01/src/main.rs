4e671ce5124e2006b4b1bf3a84b0577f89611212
Dan Stoza <dstoza@gmail.com>
2020-12-01 10:54:10 -0800

    Updated day 1 part 1 solution to avoid binary_search per candidate

--- 2020/day-01/src/main.rs
+++ 2020/day-01/src/main.rs
@@ -1,16 +1,30 @@
-use std::{env, fs::File, io::{BufRead, BufReader}};
+use std::{
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
-fn sum_product(array: Vec<i32>) -> i32 {
-    0
+fn sum_product2(sorted: &[i32], target: i32) -> i32 {
+    let mut candidate_index = sorted.len() - 1;
+    for number in sorted {
+        while number + sorted[candidate_index] > target {
+            candidate_index -= 1;
+        }
+
+        if number + sorted[candidate_index] == target {
+            return number * sorted[candidate_index];
+        }
+    }
+    -1
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 1 {
+    if args.len() < 2 {
         return;
     }
 
-    let filename = &args[0];
+    let filename = &args[1];
     let file = File::open(filename).expect(format!("Failed to open file {}", filename).as_str());
     let mut reader = BufReader::new(file);
 
@@ -19,19 +33,18 @@ fn main() {
     loop {
         let bytes = reader.read_line(&mut line).expect("Failed to read line");
         if bytes == 0 {
-            println!("bytes 0");
             break;
         }
 
-        println!("Parsing {}", &line);
+        let integer = line
+            .trim()
+            .parse::<i32>()
+            .expect(format!("Failed to parse {}", &line).as_str());
+        array.push(integer);
 
-        if let Ok(integer) = line.as_str().parse::<i32>() {
-            array.push(integer);
-        }
+        line.clear();
     }
 
-    for i in &array {
-        println!("{}", i);
-    }
-    println!("Result: {}", sum_product(array));
+    array.sort();
+    println!("Result: {}", sum_product2(&array, 2020));
 }
