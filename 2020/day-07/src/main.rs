3c2abfd1914147f072a6668da46b3af972bba230
Dan Stoza <dstoza@gmail.com>
2020-12-06 23:19:12 -0800

    Day 7: Refactor into methods like a good programmer

--- 2020/day-07/src/main.rs
+++ 2020/day-07/src/main.rs
@@ -6,32 +6,25 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-#[derive(Debug)]
 struct Containee {
     name: String,
     count: i32,
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        return;
+struct ShinyGoldTracker {
+    held_by: HashMap<String, Vec<String>>,
+    holds: HashMap<String, Vec<Containee>>,
+}
+
+impl ShinyGoldTracker {
+    fn new() -> Self {
+        Self {
+            held_by: HashMap::new(),
+            holds: HashMap::new(),
+        }
     }
 
-    let filename = &args[1];
-    let file = File::open(filename).expect(format!("Failed to open file {}", filename).as_str());
-    let mut reader = BufReader::new(file);
-
-    let mut held_by = HashMap::<String, Vec<String>>::new();
-    let mut holds = HashMap::<String, Vec<Containee>>::new();
-
-    let mut line = String::new();
-    loop {
-        let bytes = reader.read_line(&mut line).expect("Failed to read line");
-        if bytes == 0 {
-            break;
-        }
-
+    fn parse_line(&mut self, line: &str) {
         let mut split = line.split("contain");
         let container = split
             .nth(0)
@@ -63,65 +56,95 @@ fn main() {
                 })
             })
             .for_each(|containee| {
-                held_by
+                self.held_by
                     .entry(containee.name.clone())
                     .or_default()
                     .push(String::from(container));
 
-                holds
+                self.holds
                     .entry(String::from(container))
                     .or_default()
                     .push(containee);
             });
+    }
+
+    fn compute_container_count(&self) -> usize {
+        let mut work_queue = VecDeque::new();
+        work_queue.push_back("shiny gold");
+
+        let mut containers = HashSet::new();
+
+        while !work_queue.is_empty() {
+            let current = work_queue
+                .pop_front()
+                .expect("Failed to pop front of queue");
+            if let Some(parents) = self.held_by.get(current) {
+                for parent in parents {
+                    if containers.insert(parent) {
+                        work_queue.push_back(parent)
+                    }
+                }
+            }
+        }
+
+        containers.len()
+    }
+
+    fn compute_contained_count(&self) -> i32 {
+        let mut work_queue = VecDeque::new();
+        work_queue.push_back(Containee {
+            name: String::from("shiny gold"),
+            count: 1,
+        });
+
+        // Initialize to -1 so we don't count the original shiny gold bag
+        let mut contained_count = -1;
+
+        while !work_queue.is_empty() {
+            let current = work_queue
+                .pop_front()
+                .expect("Failed to pop front of queue");
+
+            contained_count += current.count;
+
+            if let Some(children) = self.holds.get(&current.name) {
+                for child in children {
+                    work_queue.push_back(Containee {
+                        name: child.name.clone(),
+                        count: current.count * child.count,
+                    });
+                }
+            }
+        }
+
+        contained_count
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        return;
+    }
+
+    let filename = &args[1];
+    let file = File::open(filename).expect(format!("Failed to open file {}", filename).as_str());
+    let mut reader = BufReader::new(file);
+
+    let mut tracker = ShinyGoldTracker::new();
+
+    let mut line = String::new();
+    loop {
+        let bytes = reader.read_line(&mut line).expect("Failed to read line");
+        if bytes == 0 {
+            break;
+        }
+
+        tracker.parse_line(&line);
 
         line.clear();
     }
 
-    let mut work_queue = VecDeque::new();
-    work_queue.push_back("shiny gold");
-
-    let mut containers = HashSet::new();
-
-    while !work_queue.is_empty() {
-        let current = work_queue
-            .pop_front()
-            .expect("Failed to pop front of queue");
-        if let Some(parents) = held_by.get(current) {
-            for parent in parents {
-                if containers.insert(parent) {
-                    work_queue.push_back(parent)
-                }
-            }
-        }
-    }
-
-    println!("Can contain shiny gold: {}", containers.len());
-
-    let mut work_queue = VecDeque::new();
-    work_queue.push_back(Containee {
-        name: String::from("shiny gold"),
-        count: 1,
-    });
-
-    // Initialize to -1 so we don't count the original shiny gold bag
-    let mut contained_count = -1;
-
-    while !work_queue.is_empty() {
-        let current = work_queue
-            .pop_front()
-            .expect("Failed to pop front of queue");
-
-        contained_count += current.count;
-
-        if let Some(children) = holds.get(&current.name) {
-            for child in children {
-                work_queue.push_back(Containee {
-                    name: child.name.clone(),
-                    count: current.count * child.count,
-                });
-            }
-        }
-    }
-
-    println!("Shiny gold contains: {}", contained_count);
+    println!("Can contain shiny gold: {}", tracker.compute_container_count());
+    println!("Shiny gold contains: {}", tracker.compute_contained_count());
 }
