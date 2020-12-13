2b6e094e98db1b4608f1be1782bfb626f0bfe921
Dan Stoza <dstoza@gmail.com>
2020-12-12 23:55:28 -0800

    Add day 13 part 2 solution

--- 2020/day-13/src/main.rs
+++ 2020/day-13/src/main.rs
@@ -1,6 +1,7 @@
 #![deny(clippy::all, clippy::pedantic)]
 
 use std::{
+    convert::TryInto,
     env,
     fs::File,
     io::{BufRead, BufReader},
@@ -33,19 +34,54 @@ fn main() {
     let (route, next_arrival) = line
         .trim()
         .split(',')
-        .filter(|route| *route != "x")
-        .map(|route_str| {
-            route_str
-                .parse::<i32>()
-                .expect("Failed to parse route as i32")
-        })
-        .map(|route| {
-            (
-                route,
-                (earliest_timestamp / route + 1) * route - earliest_timestamp,
-            )
+        .filter_map(|route| {
+            if route == "x" {
+                return None;
+            }
+
+            let route = route.parse::<i32>().expect("Failed to parse route as i32");
+            let next_arrival = (earliest_timestamp / route + 1) * route - earliest_timestamp;
+
+            Some((route, next_arrival))
         })
         .min_by_key(|(_route, next_arrival)| *next_arrival)
         .expect("Failed to find next arrival");
-    println!("{}", route * next_arrival);
+
+    println!(
+        "Next arrival {} in {} minutes (product {})",
+        route,
+        next_arrival,
+        route * next_arrival
+    );
+
+    let mut timestamp = 0;
+    let mut skip = 1;
+    for (id, modulo) in line
+        .trim()
+        .split(',')
+        .enumerate()
+        .filter_map(|(index, id)| {
+            if id == "x" {
+                return None;
+            }
+
+            let id = id.parse::<i64>().expect("Failed to parse route as i64");
+            let index: i64 = index.try_into().expect("Failed to fit index into i64");
+
+            let mut modulo = -index;
+            while modulo < 0 {
+                modulo += id;
+            }
+
+            Some((id, modulo))
+        })
+    {
+        while timestamp % id != modulo {
+            timestamp += skip;
+        }
+
+        skip *= id
+    }
+
+    println!("First subsequent timestamp: {}", timestamp);
 }
