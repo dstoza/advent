412613b7ca601520fd2eb40d9de7f20b2dcd211d
Dan Stoza <dstoza@gmail.com>
2020-12-14 08:23:24 -0800

    Add day 14 solution

--- 2020/day-14/src/main.rs
+++ 2020/day-14/src/main.rs
@@ -1,11 +1,111 @@
 #![deny(clippy::all, clippy::pedantic)]
 
 use std::{
-    convert::TryInto,
+    collections::HashMap,
     env,
     fs::File,
     io::{BufRead, BufReader},
 };
+
+enum Mode {
+    Address,
+    Value,
+}
+
+struct ProgramLoader {
+    mode: Mode,
+    set_mask: u64,
+    clear_mask: u64,
+    floating_bits: Vec<u8>,
+    memory: HashMap<u64, u64>,
+}
+
+impl ProgramLoader {
+    fn new(mode: Mode) -> Self {
+        Self {
+            mode,
+            set_mask: 0,
+            clear_mask: 0,
+            floating_bits: Vec::new(),
+            memory: HashMap::new(),
+        }
+    }
+
+    fn update_masks(&mut self, mask: &str) {
+        self.set_mask = 0;
+        self.clear_mask = 0;
+        self.floating_bits.clear();
+        let mut index = 36_u8;
+        for byte in mask.as_bytes() {
+            self.set_mask <<= 1;
+            self.clear_mask <<= 1;
+            index -= 1;
+            match *byte {
+                b'X' => self.floating_bits.push(index),
+                b'0' => self.clear_mask |= 1,
+                b'1' => self.set_mask |= 1,
+                _ => panic!("Unexpected mask byte {}", *byte),
+            }
+        }
+    }
+
+    fn write_value(&mut self, address: u64, mut floating_bits: Vec<u8>, value: u64) {
+        if floating_bits.is_empty() {
+            println!("Writing {} to {}", value, address);
+            self.memory.insert(address, value);
+            return;
+        }
+
+        let floating_bit = floating_bits.pop().expect("Failed to pop floating bit");
+        self.write_value(
+            address | 1_u64 << floating_bit,
+            floating_bits.clone(),
+            value,
+        );
+        self.write_value(address & !(1_u64 << floating_bit), floating_bits, value);
+    }
+
+    fn write_memory(&mut self, line: &str) {
+        let mut split = line.split('=');
+
+        let address = split.next().expect("Failed to get address split").trim();
+        let address: u64 = address[1..address.len() - 1]
+            .parse()
+            .expect("Failed to parse address as u64");
+
+        let value: u64 = split
+            .next()
+            .expect("Failed to get value split")
+            .trim()
+            .parse()
+            .expect("Failed to parse value as u64");
+
+        println!("floating_bits: {:?}", self.floating_bits);
+        println!("set_mask: {}", self.set_mask);
+        println!("clear_mask: {}", self.clear_mask);
+        match self.mode {
+            Mode::Address => {
+                self.write_value(address | self.set_mask, self.floating_bits.clone(), value);
+                None
+            }
+            Mode::Value => self
+                .memory
+                .insert(address, (value | self.set_mask) & !self.clear_mask),
+        };
+    }
+
+    fn parse_line(&mut self, line: &str) {
+        match &line[0..3] {
+            "mas" => self.update_masks(&line[7..]),
+            "mem" => self.write_memory(&line[3..]),
+            _ => panic!("Unexpected line [{}]", line),
+        }
+    }
+
+    fn get_memory_sum(&self) -> u64 {
+        self.memory.iter().map(|(_, value)| *value).sum()
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,71 +117,21 @@ fn main() {
     let file = File::open(filename).unwrap_or_else(|_| panic!("Failed to open file {}", filename));
     let mut reader = BufReader::new(file);
 
+    let mut loader = ProgramLoader::new(Mode::Address);
+
     let mut line = String::new();
-
-    reader
-        .read_line(&mut line)
-        .unwrap_or_else(|_| panic!("Failed to read line"));
-    let earliest_timestamp: i32 = line
-        .trim()
-        .parse()
-        .expect("Failed to read earliest timestamp");
-    line.clear();
-
-    reader
-        .read_line(&mut line)
-        .unwrap_or_else(|_| panic!("Failed to read line"));
-    let (route, next_arrival) = line
-        .trim()
-        .split(',')
-        .filter_map(|route| {
-            if route == "x" {
-                return None;
-            }
-
-            let route = route.parse::<i32>().expect("Failed to parse route as i32");
-            let next_arrival = (earliest_timestamp / route + 1) * route - earliest_timestamp;
-
-            Some((route, next_arrival))
-        })
-        .min_by_key(|(_route, next_arrival)| *next_arrival)
-        .expect("Failed to find next arrival");
-
-    println!(
-        "Next arrival {} in {} minutes (product {})",
-        route,
-        next_arrival,
-        route * next_arrival
-    );
-
-    let mut timestamp = 0;
-    let mut skip = 1;
-    for (id, modulo) in line
-        .trim()
-        .split(',')
-        .enumerate()
-        .filter_map(|(index, id)| {
-            if id == "x" {
-                return None;
-            }
-
-            let id = id.parse::<i64>().expect("Failed to parse route as i64");
-            let index: i64 = index.try_into().expect("Failed to fit index into i64");
-
-            let mut modulo = -index;
-            while modulo < 0 {
-                modulo += id;
-            }
-
-            Some((id, modulo))
-        })
-    {
-        while timestamp % id != modulo {
-            timestamp += skip;
+    loop {
+        let bytes = reader
+            .read_line(&mut line)
+            .unwrap_or_else(|_| panic!("Failed to read line"));
+        if bytes == 0 {
+            break;
         }
 
-        skip *= id
+        loader.parse_line(line.trim());
+
+        line.clear();
     }
 
-    println!("First subsequent timestamp: {}", timestamp);
+    println!("Sum: {}", loader.get_memory_sum());
 }
