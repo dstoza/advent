51714062140814202bf7905610db6da34602accb
Dan Stoza <dstoza@gmail.com>
2020-12-22 23:54:57 -0800

    Add day 23 part 2 solution

--- 2020/day-23/src/main.rs
+++ 2020/day-23/src/main.rs
@@ -3,8 +3,27 @@
 
 extern crate test;
 
+use std::{
+    cell::RefCell,
+    collections::HashMap,
+    rc::{Rc, Weak},
+};
+
 use clap::{crate_name, App, Arg};
-use common::LineReader;
+
+struct ListNode {
+    value: u32,
+    next: Weak<RefCell<ListNode>>,
+}
+
+impl ListNode {
+    fn new(value: u32) -> Self {
+        Self {
+            value,
+            next: Weak::new(),
+        }
+    }
+}
 
 fn main() {
     let args = App::new(crate_name!())
@@ -12,61 +31,112 @@ fn main() {
         .arg(Arg::from_usage("<STEPS>"))
         .get_matches();
 
-    let mut cups: Vec<_> = args
-        .value_of("INPUT")
-        .unwrap()
-        .chars()
-        .map(|character| {
-            String::from(character)
-                .parse::<u8>()
-                .expect("Failed to parse cup as u8")
-        })
-        .collect();
-    let cup_count = cups.len();
+    let mut cups = HashMap::new();
+
+    let mut max = 0;
+    let mut head = None;
+    let mut tail: Option<Rc<RefCell<ListNode>>> = None;
+    for value in args.value_of("INPUT").unwrap().chars().map(|character| {
+        String::from(character)
+            .parse::<u32>()
+            .expect("Failed to parse cup as u8")
+    }) {
+        max = max.max(value);
+        let cup = Rc::new(RefCell::new(ListNode::new(value)));
+        cups.insert(value, cup.clone());
+        if let None = head {
+            head = Some(cup.clone());
+        }
+        match &mut tail {
+            Some(tail_node) => {
+                tail_node.borrow_mut().next = Rc::downgrade(&cup);
+                tail = Some(cup.clone());
+            }
+            None => {
+                tail = Some(cup.clone());
+            }
+        }
+    }
+
+    // let cup_count = max;
+
+    let cup_count = 1_000_000;
+    for value in max + 1..=cup_count {
+        let cup = Rc::new(RefCell::new(ListNode::new(value)));
+        if let Some(tail_node) = &mut tail {
+            tail_node.borrow_mut().next = Rc::downgrade(&cup);
+            tail = Some(cup.clone());
+        }
+        cups.insert(value, cup);
+    }
+
+    // Complete the circular list
+    let head = head.unwrap();
+    let tail = tail.unwrap();
+    tail.borrow_mut().next = Rc::downgrade(&head);
 
     let steps: usize = args.value_of("STEPS").unwrap().parse().unwrap();
 
-    let mut current = 0;
-    let mut pickup = [0; 3];
+    let mut current = head.clone();
     for _ in 0..steps {
-        let mut cursor = current + 1;
-        let mut destination = (cups[current] as usize + cup_count - 2) % cup_count + 1;
-        
-        for p in 0..3 {
-            pickup[p] = cups[cursor % cup_count];
-            cursor += 1;
+        let mut picked_up = Vec::new();
+        let pickup_head = current.borrow().next.upgrade().unwrap();
+        picked_up.push(pickup_head.borrow().value);
+        let mut pickup_tail = pickup_head.borrow().next.upgrade().unwrap();
+        picked_up.push(pickup_tail.borrow().value);
+        let next = pickup_tail.borrow().next.upgrade().unwrap();
+        pickup_tail = next;
+        picked_up.push(pickup_tail.borrow().value);
+        current.borrow_mut().next = pickup_tail.borrow().next.clone();
+
+        // println!("Picked up: {:?}", picked_up);
+
+        let mut destination = (current.borrow().value + cup_count - 2) % cup_count + 1;
+        while picked_up.iter().any(|value| *value == destination) {
+            destination = (destination + cup_count - 2) % cup_count + 1
         }
 
-        while pickup
-            .iter()
-            .find(|cup| **cup as usize == destination)
-            .is_some()
-        {
-            destination = (destination + cup_count - 2) % cup_count + 1;
-        }
+        // println!("Destination: {}", destination);
 
-        // Copy elements from after the picked-up cups until we find the destination
-        cursor = (current + 1) % cup_count;
-        while cups[(cursor + 3) % cup_count] as usize != destination {
-            cups[cursor] = cups[(cursor + 3) % cup_count];
-            cursor = (cursor + 1) % cup_count;
-        }
+        let destination_node = &cups[&destination];
+        let destination_next = destination_node.borrow().next.clone();
+        destination_node.borrow_mut().next = Rc::downgrade(&pickup_head);
+        pickup_tail.borrow_mut().next = destination_next;
 
-        // Now we're at the destination, so copy it
-        cups[cursor] = cups[(cursor + 3) % cup_count];
-        cursor = (cursor + 1) % cup_count;
-
-        // Fill in the picked-up cups
-        for p in 0..3 {
-            cups[cursor] = pickup[p];
-            cursor = (cursor + 1) % cup_count;
-        }
-
-        // The remainder of the cups don't need to move
-
-        current = (current + 1) % cup_count;
+        let next = current.borrow().next.clone();
+        current = next.upgrade().unwrap();
     }
 
+    while current.borrow().value != 1 {
+        let next = current.borrow().next.upgrade().unwrap();
+        current = next;
+    }
+
+    let next = current.borrow().next.upgrade().unwrap();
+    current = next;
+
+    let mut product = 1;
+    for _ in 0..2 {
+        product *= current.borrow().value as u64;
+        let next = current.borrow().next.upgrade().unwrap();
+        current = next;
+    }
+
+    println!("Product: {}", product);
+
+    /*
+
+
+    while current.borrow().value != 1 {
+        print!("{}", current.borrow().value);
+        let next = current.borrow().next.upgrade().unwrap();
+        current = next;
+    }
+    
+    println!();
+    */
+
+    /*
     let mut cursor = 0;
     while cups[cursor] != 1 {
         cursor += 1;
@@ -77,6 +147,7 @@ fn main() {
         cursor = (cursor + 1) % cup_count;
     }
     println!();
+    */
 }
 
 #[cfg(test)]
