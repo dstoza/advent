ab96b5d45f51754e3401b934f6b7e026ea68c695
Dan Stoza <dstoza@gmail.com>
2021-12-02 23:20:38 -0800

    2021-03: Add solution

---
+++ 2021/day-03/src/main.rs
@@ -0,0 +1,145 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn calculate_power_consumption<I: Iterator<Item = String>>(lines: I) -> u32 {
+    let mut counters = Vec::new();
+    let mut total_lines = 0;
+    for line in lines {
+        for (position, bit) in line.bytes().enumerate() {
+            if counters.len() <= position {
+                counters.push(0);
+            }
+
+            counters[position] += (bit - b'0') as u16;
+        }
+        total_lines += 1;
+    }
+
+    let num_counters = counters.len();
+    let gamma_rate = counters.into_iter().fold(0, |value, place_count| {
+        value * 2 + (place_count > total_lines / 2) as u32
+    });
+    let epsilon_rate = !gamma_rate & ((1u32 << num_counters) - 1);
+    gamma_rate * epsilon_rate
+}
+
+fn calculate_first_position(values: &[u16]) -> u16 {
+    let leading_zeros = values.last().unwrap().leading_zeros();
+    1u16 << (15 - leading_zeros)
+}
+
+fn calculate_oxygen_generator_rating(values: &[u16]) -> u16 {
+    let mut start = 0;
+    let mut end = values.len() - 1;
+    let mut position = calculate_first_position(values);
+    while start != end {
+        let middle = start + (end - start) / 2;
+        if (values[middle] & position) != 0 {
+            while (values[start] & position) == 0 && start != end {
+                start += 1;
+            }
+        } else if (end - start) % 2 == 1 && (values[middle + 1] & position) != 0 {
+            // Tiebreaker if there are an equal number of elements
+            start = middle + 1;
+        } else {
+            while (values[end] & position) != 0 && end != start {
+                end -= 1;
+            }
+        }
+        position >>= 1;
+    }
+    values[start]
+}
+
+fn calculate_co2_scrubber_rating(values: &[u16]) -> u16 {
+    let mut start = 0;
+    let mut end = values.len() - 1;
+    let mut position = calculate_first_position(values);
+    while start != end {
+        let middle = start + (end - start) / 2;
+        if (values[middle] & position) != 0 {
+            while (values[end] & position) != 0 && end != start {
+                end -= 1;
+            }
+        } else if (end - start) % 2 == 1 && (values[middle + 1] & position) != 0 {
+            // Tiebreaker if there are an equal number of elements
+            end = middle;
+        } else {
+            while (values[start] & position) == 0 && start != end {
+                start += 1;
+            }
+        }
+        position >>= 1;
+    }
+    values[start]
+}
+
+fn calculate_life_support_rating<I: Iterator<Item = String>>(lines: I) -> u32 {
+    let mut values: Vec<u16> = lines
+        .map(|s| u16::from_str_radix(s.as_ref(), 2).unwrap())
+        .collect();
+    values.sort_unstable();
+    calculate_oxygen_generator_rating(values.as_ref()) as u32
+        * calculate_co2_scrubber_rating(values.as_ref()) as u32
+}
+
+fn main() {
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+    println!(
+        "Life support rating: {}",
+        calculate_life_support_rating(reader.lines().into_iter().map(|line| line.unwrap()))
+    );
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn get_sample_input() -> [String; 12] {
+        [
+            String::from("00100"),
+            String::from("11110"),
+            String::from("10110"),
+            String::from("10111"),
+            String::from("10101"),
+            String::from("01111"),
+            String::from("00111"),
+            String::from("11100"),
+            String::from("10000"),
+            String::from("11001"),
+            String::from("00010"),
+            String::from("01010"),
+        ]
+    }
+
+    #[test]
+    fn test_power_consumption() {
+        assert_eq!(
+            calculate_power_consumption(get_sample_input().into_iter()),
+            198
+        );
+    }
+
+    #[test]
+    fn test_oxyten_generator_rating() {
+        let mut values: Vec<u16> = get_sample_input()
+            .into_iter()
+            .map(|s| u16::from_str_radix(s.as_ref(), 2).unwrap())
+            .collect();
+        values.sort();
+        assert_eq!(calculate_oxygen_generator_rating(values.as_ref()), 23);
+    }
+
+    #[test]
+    fn test_co2_scrubber_rating() {
+        let mut values: Vec<u16> = get_sample_input()
+            .into_iter()
+            .map(|s| u16::from_str_radix(s.as_ref(), 2).unwrap())
+            .collect();
+        values.sort();
+        assert_eq!(calculate_co2_scrubber_rating(values.as_ref()), 10);
+    }
+}
