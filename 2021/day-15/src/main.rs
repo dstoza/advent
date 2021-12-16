91d910816dd148863190e6311886c01d8406e0ce
Dan Stoza <dstoza@gmail.com>
2021-12-15 16:21:41 -0800

    2021-15: More A* cleanup

--- 2021/day-15/src/main.rs
+++ 2021/day-15/src/main.rs
@@ -5,7 +5,6 @@ use std::{
     collections::{BinaryHeap, VecDeque},
     fs::File,
     io::{BufRead, BufReader},
-    rc::Rc,
 };
 
 fn parse_input<I: Iterator<Item = String>>(lines: I) -> Vec<Vec<u8>> {
@@ -168,7 +167,7 @@ fn expand_map(risk_to_enter: &[Vec<u8>]) -> Vec<Vec<u8>> {
 
 #[derive(Debug, Eq)]
 struct Node {
-    previous: Option<Rc<Node>>,
+    previous: Vec<(u16, u16)>,
     total_estimated_risk: u16,
     risk_to_node: u16,
     row: u16,
@@ -176,17 +175,8 @@ struct Node {
 }
 
 impl Node {
-    fn has_previous(&self, row: u16, column: u16) -> bool {
-        return match &self.previous {
-            Some(previous) => {
-                if previous.row == row && previous.column == column {
-                    true
-                } else {
-                    previous.has_previous(row, column)
-                }
-            }
-            None => false,
-        };
+    fn has_previous(&self, row: usize, column: usize) -> bool {
+        self.previous.contains(&(row as u16, column as u16))
     }
 }
 
@@ -208,107 +198,98 @@ impl PartialEq for Node {
     }
 }
 
-fn get_estimated_risk(from_row: u16, from_column: u16, to_index: u16) -> u16 {
-    to_index - from_row + to_index - from_column
+fn get_estimated_risk(from_row: usize, from_column: usize, to_index: usize) -> u16 {
+    (to_index - from_row + to_index - from_column) as u16
 }
 
-fn a_star_lowest_risk(risk_to_enter: &[Vec<u8>]) -> u16 {
+fn push_neighbor(
+    risk_to_enter: &[Vec<u8>],
+    lowest_risk: &mut [Vec<u16>],
+    queue: &mut BinaryHeap<Node>,
+    node: &Node,
+    row: usize,
+    column: usize,
+) {
+    let risk_to_neighbor = node.risk_to_node + risk_to_enter[row][column] as u16;
+    if risk_to_neighbor < lowest_risk[row][column] && !node.has_previous(row, column) {
+        lowest_risk[row][column] = risk_to_neighbor;
+        let previous = {
+            let mut previous = node.previous.clone();
+            previous.push((node.row, node.column));
+            previous
+        };
+        queue.push(Node {
+            previous,
+            total_estimated_risk: risk_to_neighbor
+                + get_estimated_risk(row, column, risk_to_enter.len() - 1),
+            risk_to_node: risk_to_neighbor,
+            row: row as u16,
+            column: column as u16,
+        });
+    }
+}
+
+fn get_lowest_risk_a_star(risk_to_enter: &[Vec<u8>]) -> u16 {
+    let to_index = risk_to_enter.len() - 1;
+
     let mut lowest_risk = vec![vec![u16::MAX; risk_to_enter.len()]; risk_to_enter.len()];
 
-    let to_index = (risk_to_enter.len() - 1) as u16;
-
     let mut queue = BinaryHeap::new();
-    queue.push(Rc::new(Node {
-        previous: None,
+    queue.push(Node {
+        previous: Vec::new(),
         total_estimated_risk: get_estimated_risk(0, 0, to_index),
         risk_to_node: 0,
         row: 0,
         column: 0,
-    }));
-
-    let mut hits = 0;
-    let mut misses = 0;
+    });
 
     while let Some(node) = queue.pop() {
-        if node.row == to_index && node.column == to_index {
-            println!("Hits: {} Misses: {}", hits, misses);
+        if node.row as usize == to_index && node.column as usize == to_index {
             return node.risk_to_node;
         }
 
         if node.row > 0 {
-            let risk_to_node = node.risk_to_node
-                + risk_to_enter[(node.row - 1) as usize][node.column as usize] as u16;
-            if risk_to_node < lowest_risk[(node.row - 1) as usize][node.column as usize] && !node.has_previous(node.row - 1, node.column) {
-                lowest_risk[(node.row - 1) as usize][node.column as usize] = risk_to_node;
-                queue.push(Rc::new(Node {
-                    previous: Some(node.clone()),
-                    total_estimated_risk: risk_to_node
-                        + get_estimated_risk(node.row - 1, node.column, to_index),
-                    risk_to_node,
-                    row: node.row - 1,
-                    column: node.column,
-                }));
-                hits += 1
-            } else {
-                misses += 1;
-            }
+            push_neighbor(
+                risk_to_enter,
+                &mut lowest_risk,
+                &mut queue,
+                &node,
+                (node.row - 1) as usize,
+                node.column as usize,
+            );
         }
 
         if node.column > 0 {
-            let risk_to_node = node.risk_to_node
-                + risk_to_enter[node.row as usize][(node.column - 1) as usize] as u16;
-            if risk_to_node < lowest_risk[node.row as usize][(node.column - 1) as usize] && !node.has_previous(node.row, node.column - 1) {
-                lowest_risk[node.row as usize][(node.column - 1) as usize] = risk_to_node;
-                queue.push(Rc::new(Node {
-                    previous: Some(node.clone()),
-                    total_estimated_risk: risk_to_node
-                        + get_estimated_risk(node.row, node.column - 1, to_index),
-                    risk_to_node,
-                    row: node.row,
-                    column: node.column - 1,
-                }));
-                hits += 1
-            } else {
-                misses += 1;
-            }
+            push_neighbor(
+                risk_to_enter,
+                &mut lowest_risk,
+                &mut queue,
+                &node,
+                node.row as usize,
+                (node.column - 1) as usize,
+            );
         }
 
-        if node.column < to_index {
-            let risk_to_node = node.risk_to_node
-                + risk_to_enter[node.row as usize][(node.column + 1) as usize] as u16;
-            if risk_to_node < lowest_risk[node.row as usize][(node.column + 1) as usize] && !node.has_previous(node.row, node.column + 1) {
-                lowest_risk[node.row as usize][(node.column + 1) as usize] = risk_to_node;
-                queue.push(Rc::new(Node {
-                    previous: Some(node.clone()),
-                    total_estimated_risk: risk_to_node
-                        + get_estimated_risk(node.row, node.column + 1, to_index),
-                    risk_to_node,
-                    row: node.row,
-                    column: node.column + 1,
-                }));
-                hits += 1
-            } else {
-                misses += 1;
-            }
+        if (node.column as usize) < to_index {
+            push_neighbor(
+                risk_to_enter,
+                &mut lowest_risk,
+                &mut queue,
+                &node,
+                node.row as usize,
+                (node.column + 1) as usize,
+            );
         }
 
-        if node.row < to_index {
-            let risk_to_node = node.risk_to_node
-                + risk_to_enter[(node.row + 1) as usize][node.column as usize] as u16;
-            if risk_to_node < lowest_risk[(node.row + 1) as usize][node.column as usize] && !node.has_previous(node.row + 1, node.column) {
-                lowest_risk[(node.row + 1) as usize][node.column as usize] = risk_to_node;
-                queue.push(Rc::new(Node {
-                    previous: Some(node.clone()),
-                    total_estimated_risk: risk_to_node
-                        + get_estimated_risk(node.row + 1, node.column, to_index),
-                    risk_to_node,
-                    row: node.row + 1,
-                    column: node.column,
-                }));
-                hits += 1
-            } else {
-                misses += 1;
-            }
+        if (node.row as usize) < to_index {
+            push_neighbor(
+                risk_to_enter,
+                &mut lowest_risk,
+                &mut queue,
+                &node,
+                (node.row + 1) as usize,
+                node.column as usize,
+            );
         }
     }
 
@@ -320,7 +301,10 @@ fn main() {
     let reader = BufReader::new(file);
     let risk_to_enter = parse_input(reader.lines().map(|line| line.unwrap()));
     // println!("Lowest risk: {}", get_lowest_risk(&risk_to_enter));
-    println!("Lowest risk: {}", a_star_lowest_risk(&(expand_map(&risk_to_enter))));
+    println!(
+        "Lowest risk: {}",
+        get_lowest_risk(&(expand_map(&risk_to_enter)))
+    );
 }
 
 #[cfg(test)]
@@ -351,15 +335,15 @@ mod tests {
     }
 
     #[test]
-    fn test_lowest_cost() {
+    fn test_lowest_risk() {
         let risk_to_enter = parse_input(get_example().into_iter());
         assert_eq!(get_lowest_risk(&risk_to_enter), 40);
     }
 
     #[test]
-    fn test_a_star() {
+    fn test_lowest_risk_a_star() {
         let risk_to_enter = parse_input(get_example().into_iter());
-        assert_eq!(a_star_lowest_risk(&risk_to_enter), 40);
+        assert_eq!(get_lowest_risk_a_star(&risk_to_enter), 40);
     }
 
     #[test]
@@ -391,15 +375,15 @@ mod tests {
     }
 
     #[test]
-    fn test_lowest_expanded_cost() {
+    fn test_lowest_risk_expanded() {
         let risk_to_enter = parse_input(get_example().into_iter());
         assert_eq!(get_lowest_risk(&expand_map(&risk_to_enter)), 315);
     }
 
     #[test]
-    fn test_lowest_expanded_a_star() {
+    fn test_lowest_risk_expanded_a_star() {
         let risk_to_enter = parse_input(get_example().into_iter());
-        assert_eq!(a_star_lowest_risk(&expand_map(&risk_to_enter)), 315);
+        assert_eq!(get_lowest_risk_a_star(&expand_map(&risk_to_enter)), 315);
     }
 
     #[bench]
@@ -415,14 +399,14 @@ mod tests {
     }
 
     #[bench]
-    fn bench_a_star(b: &mut Bencher) {
+    fn bench_input_a_star(b: &mut Bencher) {
         let file = File::open("input.txt").unwrap();
         let reader = BufReader::new(file);
         let lines: Vec<_> = reader.lines().map(|line| line.unwrap()).collect();
 
         b.iter(|| {
             let risk_to_enter = parse_input(lines.clone().into_iter());
-            assert_eq!(a_star_lowest_risk(&expand_map(&risk_to_enter)), 2814);
+            assert_eq!(get_lowest_risk_a_star(&expand_map(&risk_to_enter)), 2814);
         })
     }
 }
