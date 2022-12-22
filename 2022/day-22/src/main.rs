dc3351dabb642ea0e2c65a1f0a80a4ecdd75b217
Dan Stoza <dstoza@gmail.com>
2022-12-22 13:43:23 -0800

    2022-22: Add board parsing

--- 2022/day-22/src/main.rs
+++ 2022/day-22/src/main.rs
@@ -43,6 +43,34 @@ impl Position {
     }
 }
 
+fn parse_board(lines: impl Iterator<Item = String>) -> Vec<Vec<u8>> {
+    let mut board = vec![Vec::new()];
+    let mut max_length = 0;
+    for line in lines {
+        if line.is_empty() {
+            break;
+        }
+
+        board.push(
+            " ".as_bytes()
+                .iter()
+                .chain(line.as_bytes())
+                .chain(" ".as_bytes().iter())
+                .copied()
+                .collect(),
+        );
+        max_length = max_length.max(board.last().unwrap().len());
+    }
+
+    for row in &mut board {
+        row.resize(max_length, b' ');
+    }
+
+    board.push(vec![b' '; max_length]);
+
+    board
+}
+
 fn main() {
     let filename = std::env::args().nth(1).expect("Filename not found");
 
@@ -50,4 +78,8 @@ fn main() {
         File::open(&filename).unwrap_or_else(|_| panic!("Couldn't open {}", filename.as_str()));
     let reader = BufReader::new(file);
     let lines = reader.lines().map(std::result::Result::unwrap);
+    let board = parse_board(lines);
+    for line in board {
+        println!("{:?}", line);
+    }
 }
