c015a47c34f1ed1671ed91fd82c4c7f420db68fc
Dan Stoza <dstoza@gmail.com>
2022-12-22 16:41:30 -0800

    2022-22: Add part 1 solution

--- 2022/day-22/src/main.rs
+++ 2022/day-22/src/main.rs
@@ -1,3 +1,4 @@
+#![feature(iter_intersperse)]
 #![warn(clippy::pedantic)]
 
 use std::{
@@ -7,6 +8,7 @@ use std::{
     iter::Iterator,
 };
 
+#[derive(Clone, Copy, Debug)]
 enum Turn {
     Right = 1,
     Left = 3,
@@ -30,6 +32,15 @@ impl Facing {
             _ => unreachable!(),
         }
     }
+
+    fn opposite(self) -> Self {
+        match self {
+            Self::Right => Self::Left,
+            Self::Down => Self::Up,
+            Self::Left => Self::Right,
+            Self::Up => Self::Down,
+        }
+    }
 }
 
 struct WrapCache {
@@ -37,18 +48,35 @@ struct WrapCache {
 }
 
 impl WrapCache {
-    fn next(&mut self, position: Position, facing: Facing) -> Option<Position> {
+    fn new() -> Self {
+        Self {
+            cache: HashMap::new(),
+        }
+    }
+
+    fn next(&mut self, board: &[Vec<u8>], position: Position, facing: Facing) -> Option<Position> {
         if let Some(hit) = self.cache.get(&(position, facing)) {
             return *hit;
         }
 
-        // TODO: Step backwards until we find a blank space, then forwards one, check if it's a wall, then store/return the result
+        let mut search = position.step(facing.opposite());
+        while board[search.row][search.column] != b' ' {
+            search = search.step(facing.opposite());
+        }
+        search = search.step(facing);
 
-        None
+        let result = match board[search.row][search.column] {
+            b'#' => None,
+            b'.' => Some(search),
+            _ => unimplemented!(),
+        };
+
+        self.cache.insert((position, facing), result);
+        result
     }
 }
 
-#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct Position {
     row: usize,
     column: usize,
@@ -82,7 +110,7 @@ impl Position {
         match board[next_coordinates.row][next_coordinates.column] {
             b'#' => None,
             b'.' => Some(next_coordinates),
-            b' ' => wrap_cache.next(self, facing),
+            b' ' => wrap_cache.next(board, self, facing),
             _ => unimplemented!(),
         }
     }
@@ -116,15 +144,66 @@ fn parse_board(lines: impl Iterator<Item = String>) -> Vec<Vec<u8>> {
     board
 }
 
+#[derive(Debug)]
+enum Command {
+    Step(usize),
+    Turn(Turn),
+}
+
+fn parse_commands(line: String) -> Vec<Command> {
+    line.split('R')
+        .intersperse("R")
+        .flat_map(|s| s.split('L').intersperse("L"))
+        .map(|s| match s {
+            "R" => Command::Turn(Turn::Right),
+            "L" => Command::Turn(Turn::Left),
+            _ => Command::Step(s.parse().unwrap()),
+        })
+        .collect()
+}
+
+fn run_commands(commands: &[Command], board: &[Vec<u8>]) {
+    let mut wrap_cache = WrapCache::new();
+
+    let column = board[1].iter().position(|b| *b == b'.').unwrap();
+    let mut position = Position { row: 1, column };
+    let mut facing = Facing::Right;
+
+    for command in commands {
+        match command {
+            Command::Turn(turn) => facing = facing.turn(*turn),
+            Command::Step(steps) => {
+                for _ in 0..*steps {
+                    let next_position = position.next(board, &mut wrap_cache, facing);
+                    if let Some(next_position) = next_position {
+                        println!("{:?}", next_position);
+                        position = next_position;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    let password = 1000 * position.row + 4 * position.column + facing as usize;
+    println!("Password: {password}");
+}
+
 fn main() {
     let filename = std::env::args().nth(1).expect("Filename not found");
 
     let file =
         File::open(&filename).unwrap_or_else(|_| panic!("Couldn't open {}", filename.as_str()));
     let reader = BufReader::new(file);
-    let lines = reader.lines().map(std::result::Result::unwrap);
-    let board = parse_board(lines);
-    for line in board {
+    let mut lines = reader.lines().map(std::result::Result::unwrap);
+    let board = parse_board(&mut lines);
+    for line in &board {
         println!("{:?}", line);
     }
+
+    let commands = parse_commands(lines.next().unwrap());
+    println!("{:?}", commands);
+
+    run_commands(&commands, &board);
 }
