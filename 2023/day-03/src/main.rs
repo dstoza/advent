a3b917e05c35d8b8bd9cd003d3a9a88f25777581
Dan Stoza <dstoza@gmail.com>
2023-12-02 13:28:17 -0800

    2023-03: Add part 2 solution

--- 2023/day-03/src/main.rs
+++ 2023/day-03/src/main.rs
@@ -33,18 +33,32 @@ fn main() {
     let mut sum = 0;
     let mut value = None;
     let mut adjacent = false;
+    let mut adjacent_asterisks = Vec::new();
+    let mut adjacent_values = HashMap::new();
     for (row, chunk) in board.chunks(width).enumerate() {
         for (column, byte) in chunk.iter().enumerate() {
             if byte.is_ascii_digit() {
                 if value.is_none() {
                     adjacent |=
                         (row - 1..=row + 1).any(|r| is_symbol(board[r * width + column - 1]));
+
+                    for r in row - 1..=row + 1 {
+                        if board[r * width + column - 1] == b'*' {
+                            adjacent_asterisks.push((r, column - 1));
+                        }
+                    }
                 }
 
                 if is_symbol(board[(row - 1) * width + column])
                     || is_symbol(board[(row + 1) * width + column])
                 {
                     adjacent = true;
+                }
+
+                for r in row - 1..=row + 1 {
+                    if board[r * width + column] == b'*' {
+                        adjacent_asterisks.push((r, column));
+                    }
                 }
 
                 value = Some(value.map_or_else(
@@ -58,11 +72,38 @@ fn main() {
                     sum += v;
                 }
 
+                for r in row - 1..=row + 1 {
+                    if board[r * width + column] == b'*' {
+                        adjacent_asterisks.push((r, column));
+                    }
+                }
+
+                for asterisk in &adjacent_asterisks {
+                    adjacent_values
+                        .entry(*asterisk)
+                        .and_modify(|values: &mut Vec<u32>| values.push(v))
+                        .or_insert_with(|| vec![v]);
+                }
+
                 value = None;
                 adjacent = false;
+                adjacent_asterisks.clear();
             }
         }
     }
 
     println!("{sum}");
+
+    let gear_sum: u32 = adjacent_values
+        .values()
+        .filter_map(|values| {
+            if values.len() == 2 {
+                Some(values.iter().product::<u32>())
+            } else {
+                None
+            }
+        })
+        .sum();
+
+    println!("{gear_sum}");
 }
