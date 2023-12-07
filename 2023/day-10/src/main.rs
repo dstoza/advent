fb0a06e3672a1144b247bab1422e0211e9c91e4e
Dan Stoza <dstoza@gmail.com>
2023-12-07 08:42:07 -0800

    2023-10: Add part 2 solution

--- 2023/day-10/src/main.rs
+++ 2023/day-10/src/main.rs
@@ -9,9 +9,13 @@ use std::{
 #[derive(Clone, Copy, Debug)]
 enum Direction {
     North,
+    NorthEast,
     East,
+    SouthEast,
     South,
+    SouthWest,
     West,
+    NorthWest,
 }
 
 impl Direction {
@@ -25,6 +29,7 @@ impl Direction {
             Self::East => Self::West,
             Self::South => Self::North,
             Self::West => Self::East,
+            _ => unimplemented!(),
         }
     }
 }
@@ -49,11 +54,33 @@ impl Coordinates {
                     None
                 }
             }
+            Direction::NorthEast => {
+                if self.row > 0 {
+                    Some(Coordinates::new(self.row - 1, self.column + 1))
+                } else {
+                    None
+                }
+            }
             Direction::East => Some(Coordinates::new(self.row, self.column + 1)),
+            Direction::SouthEast => Some(Coordinates::new(self.row + 1, self.column + 1)),
             Direction::South => Some(Coordinates::new(self.row + 1, self.column)),
+            Direction::SouthWest => {
+                if self.column > 0 {
+                    Some(Coordinates::new(self.row + 1, self.column - 1))
+                } else {
+                    None
+                }
+            }
             Direction::West => {
                 if self.column > 0 {
                     Some(Coordinates::new(self.row, self.column - 1))
+                } else {
+                    None
+                }
+            }
+            Direction::NorthWest => {
+                if self.row > 0 && self.column > 0 {
+                    Some(Coordinates::new(self.row - 1, self.column - 1))
                 } else {
                     None
                 }
@@ -62,16 +89,34 @@ impl Coordinates {
     }
 }
 
-trait Map {
+trait At {
     fn at(&self, coordinates: Coordinates) -> Option<u8>;
 }
 
-impl Map for &[Vec<u8>] {
+impl At for &[Vec<u8>] {
     fn at(&self, coordinates: Coordinates) -> Option<u8> {
         if coordinates.row < self.len() && coordinates.column < self[coordinates.row].len() {
             Some(self[coordinates.row][coordinates.column])
         } else {
             None
+        }
+    }
+}
+
+impl At for &mut [Vec<u8>] {
+    fn at(&self, coordinates: Coordinates) -> Option<u8> {
+        (&(**self)).at(coordinates)
+    }
+}
+
+trait Set {
+    fn set(&mut self, coordinates: Coordinates, value: u8);
+}
+
+impl Set for &mut [Vec<u8>] {
+    fn set(&mut self, coordinates: Coordinates, value: u8) {
+        if coordinates.row < self.len() && coordinates.column < self[coordinates.row].len() {
+            self[coordinates.row][coordinates.column] = value;
         }
     }
 }
@@ -153,10 +198,11 @@ fn pipe_direction(value: u8, from: Direction) -> Option<Direction> {
 }
 
 impl<'a> Iterator for PipeIterator<'a> {
-    type Item = Coordinates;
+    type Item = (Coordinates, Direction);
 
-    fn next(&mut self) -> Option<Coordinates> {
+    fn next(&mut self) -> Option<(Coordinates, Direction)> {
         let next = self.next;
+        let from = self.from;
         let Some(value) = self.grid.at(self.next) else {
             return None;
         };
@@ -168,7 +214,134 @@ impl<'a> Iterator for PipeIterator<'a> {
         };
         self.next = new_next;
         self.from = direction.opposite();
-        Some(next)
+        Some((next, from))
+    }
+}
+
+fn maybe_set(mut grid: &mut [Vec<u8>], coordinates: Option<Coordinates>, value: u8) {
+    let Some(coordinates) = coordinates else {
+        return;
+    };
+
+    if let Some(b'.') = grid.at(coordinates) {
+        grid.set(coordinates, value);
+    }
+}
+
+fn fill_tracker(mut grid: &mut [Vec<u8>], coordinates: Coordinates, value: u8, from: Direction) {
+    grid.set(coordinates, b'*');
+
+    match value {
+        b'|' => {
+            let (a, b) = match from {
+                Direction::North => (b'L', b'R'),
+                Direction::South => (b'R', b'L'),
+                _ => unreachable!(),
+            };
+            for direction in [Direction::NorthEast, Direction::East, Direction::SouthEast] {
+                maybe_set(grid, coordinates.step(direction), a);
+            }
+            for direction in [Direction::NorthWest, Direction::West, Direction::SouthWest] {
+                maybe_set(grid, coordinates.step(direction), b);
+            }
+        }
+        b'-' => {
+            let (a, b) = match from {
+                Direction::West => (b'L', b'R'),
+                Direction::East => (b'R', b'L'),
+                _ => unreachable!(),
+            };
+            for direction in [Direction::NorthWest, Direction::North, Direction::NorthEast] {
+                maybe_set(grid, coordinates.step(direction), a);
+            }
+            for direction in [Direction::SouthWest, Direction::South, Direction::SouthEast] {
+                maybe_set(grid, coordinates.step(direction), b);
+            }
+        }
+        b'L' => {
+            let (a, b) = match from {
+                Direction::North => (b'L', b'R'),
+                Direction::East => (b'R', b'L'),
+                _ => unreachable!(),
+            };
+            maybe_set(grid, coordinates.step(Direction::NorthEast), a);
+            for direction in [
+                Direction::SouthEast,
+                Direction::South,
+                Direction::SouthWest,
+                Direction::West,
+                Direction::NorthWest,
+            ] {
+                maybe_set(grid, coordinates.step(direction), b);
+            }
+        }
+        b'J' => {
+            let (a, b) = match from {
+                Direction::North => (b'L', b'R'),
+                Direction::West => (b'R', b'L'),
+                _ => unreachable!(),
+            };
+            for direction in [
+                Direction::NorthEast,
+                Direction::East,
+                Direction::SouthEast,
+                Direction::South,
+                Direction::SouthWest,
+            ] {
+                maybe_set(grid, coordinates.step(direction), a);
+            }
+            maybe_set(grid, coordinates.step(Direction::NorthWest), b);
+        }
+        b'7' => {
+            let (a, b) = match from {
+                Direction::West => (b'L', b'R'),
+                Direction::South => (b'R', b'L'),
+                _ => unreachable!(),
+            };
+            for direction in [
+                Direction::NorthWest,
+                Direction::North,
+                Direction::NorthEast,
+                Direction::East,
+                Direction::SouthEast,
+            ] {
+                maybe_set(grid, coordinates.step(direction), a);
+            }
+            maybe_set(grid, coordinates.step(Direction::SouthWest), b);
+        }
+        b'F' => {
+            let (a, b) = match from {
+                Direction::East => (b'L', b'R'),
+                Direction::South => (b'R', b'L'),
+                _ => unreachable!(),
+            };
+            maybe_set(grid, coordinates.step(Direction::SouthEast), a);
+            for direction in [
+                Direction::SouthWest,
+                Direction::West,
+                Direction::NorthWest,
+                Direction::North,
+                Direction::NorthEast,
+            ] {
+                maybe_set(grid, coordinates.step(direction), b);
+            }
+        }
+        _ => (),
+    }
+}
+
+fn expand(tracker: &mut [Vec<u8>]) {
+    for row in 0..tracker.len() {
+        for column in 0..tracker[0].len() {
+            let coordinates = Coordinates::new(row, column);
+            let value = tracker.at(coordinates).unwrap();
+            if value == b'L' || value == b'R' {
+                maybe_set(tracker, coordinates.step(Direction::North), value);
+                maybe_set(tracker, coordinates.step(Direction::East), value);
+                maybe_set(tracker, coordinates.step(Direction::South), value);
+                maybe_set(tracker, coordinates.step(Direction::West), value);
+            }
+        }
     }
 }
 
@@ -199,7 +372,7 @@ fn main() {
 
     let mut steps = 0;
     let mut visited = HashSet::new();
-    for (a, b) in iterator {
+    for ((a, _), (b, _)) in iterator {
         steps += 1;
         if !visited.insert(a) || !visited.insert(b) {
             break;
@@ -207,4 +380,41 @@ fn main() {
     }
 
     println!("{steps}");
+
+    let mut tracker = vec![vec![b'.'; grid[0].len()]; grid.len()];
+    tracker.as_mut_slice().set(start, b'*');
+
+    for (coordinates, from) in iterators[0].clone() {
+        fill_tracker(
+            &mut tracker,
+            coordinates,
+            grid.as_slice().at(coordinates).unwrap(),
+            from,
+        );
+    }
+
+    // for line in &tracker {
+    //     for byte in line {
+    //         print!("{}", *byte as char);
+    //     }
+    //     println!();
+    // }
+
+    for _ in 0..10 {
+        expand(&mut tracker);
+    }
+
+    // for line in &tracker {
+    //     for byte in line {
+    //         print!("{}", *byte as char);
+    //     }
+    //     println!();
+    // }
+
+    let inside = tracker
+        .iter()
+        .flat_map(|line| line.iter())
+        .filter(|b| **b == b'L')
+        .count();
+    println!("{inside}");
 }
