5896e43b1c0fbd1f68ee1191bc35c16195c2a6a6
Dan Stoza <dstoza@gmail.com>
2023-12-12 12:57:02 -0800

    2023-12: Change algorithm to something easier to cache

--- 2023/day-12/src/main.rs
+++ 2023/day-12/src/main.rs
@@ -2,130 +2,143 @@
 use std::{
     fs::File,
     io::{BufRead, BufReader},
-    ops::RangeInclusive,
 };
 
-enum Match {
-    Exact,
-    Possible,
-    None,
-}
+fn count_segment_arrangements(segment: &[u8], lengths: &[usize], depth: usize) -> usize {
+    // println!(
+    //     "{depth} count {} {lengths:?}",
+    //     std::str::from_utf8(segment).unwrap()
+    // );
 
-#[derive(Debug)]
-enum Status {
-    Exact(usize),
-    Range(RangeInclusive<usize>),
-}
+    if segment.iter().any(|b| *b == b'#') && lengths.is_empty() {
+        // println!("{depth} a returning 0");
+        return 0;
+    }
 
-impl Status {
-    fn from_damaged(damaged: &[u8]) -> Vec<Self> {
-        if damaged.is_empty() {
-            return Vec::new();
-        }
+    if lengths.is_empty() {
+        // println!("{depth} b returning 1");
+        return 1;
+    }
 
-        let damaged = std::str::from_utf8(damaged)
-            .unwrap()
-            .trim_start_matches('.')
-            .trim_end_matches('.')
-            .as_bytes();
+    if lengths.iter().copied().sum::<usize>() + lengths.len() - 1 > segment.len() {
+        // println!("{depth} c returning 0");
+        return 0;
+    }
 
-        let minimum = if damaged.starts_with(&[b'#']) {
-            match damaged.iter().position(|b| *b == b'.' || *b == b'?') {
-                Some(position) => position,
-                None => damaged.len(),
+    let first_length = lengths[0];
+    let arrangements = (0..=segment.len() - first_length)
+        .filter_map(|start| {
+            if start + first_length < segment.len() && segment[start + first_length] == b'#' {
+                return None;
             }
-        } else {
-            0
-        };
 
-        let maximum = match damaged[minimum..].iter().position(|b| *b == b'.') {
-            Some(position) => minimum + position,
-            None => minimum + damaged[minimum..].len(),
-        };
-
-        let mut result = if minimum == maximum {
-            vec![Self::Exact(minimum)]
-        } else {
-            vec![Self::Range(minimum..=maximum)]
-        };
-
-        result.append(&mut Self::from_damaged(&damaged[maximum..]));
-
-        result
-    }
-
-    fn from_known(known: &str) -> Vec<Self> {
-        known
-            .split(',')
-            .map(|length| Self::Exact(length.parse().unwrap()))
-            .collect()
-    }
-
-    fn get_match(damaged: &[Self], known: &[Self]) -> Match {
-        for (damaged, known) in damaged.iter().zip(known.iter()) {
-            match (damaged, known) {
-                (Self::Exact(d), Self::Exact(k)) => {
-                    if *d != *k {
-                        return Match::None;
-                    }
-                }
-                (Self::Range(range), Self::Exact(known)) => {
-                    if *range.start() == 0 || range.contains(known) {
-                        return Match::Possible;
-                    }
-                    return Match::None;
-                }
-                _ => (),
+            if segment[0..start].iter().any(|b| *b == b'#') {
+                return None;
             }
-        }
 
-        if damaged.len() == known.len() {
-            Match::Exact
-        } else if damaged.len() > known.len()
-            && damaged.iter().any(|d| matches!(d, Status::Range(_)))
-        {
-            Match::Possible
-        } else {
-            Match::None
-        }
-    }
+            let remainder = if start + first_length + 1 < segment.len() {
+                &segment[start + first_length + 1..]
+            } else {
+                &[]
+            };
+
+            Some(count_segment_arrangements(
+                remainder,
+                &lengths[1..],
+                depth + 1,
+            ))
+        })
+        .collect::<Vec<_>>();
+
+    // println!("{arrangements:?}");
+
+    let sum = arrangements.iter().sum();
+
+    // println!("{depth} d returning {sum}");
+    sum
 }
 
-fn replace_first_unknown(damaged: &[u8], with: u8) -> Vec<u8> {
-    if let Some(position) = damaged.iter().position(|b| *b == b'?') {
-        let mut replaced = Vec::from(&damaged[..position]);
-        replaced.push(with);
-        replaced.extend_from_slice(&damaged[position + 1..]);
-        replaced
-    } else {
-        Vec::from(damaged)
-    }
-}
+fn count_arrangements(segments: &[Vec<u8>], lengths: &[usize]) -> usize {
+    // println!(
+    //     "count_arrangements {:?} {lengths:?}",
+    //     segments
+    //         .iter()
+    //         .map(|segment| String::from_utf8(segment.clone()).unwrap())
+    //         .collect::<Vec<_>>()
+    // );
 
-fn count_possibilities(damaged: &[u8], known: &[Status]) -> usize {
-    let damaged_status = Status::from_damaged(damaged);
-    match Status::get_match(&damaged_status, known) {
-        Match::Exact => 1,
-        Match::None => 0,
-        Match::Possible => {
-            let with_operational = replace_first_unknown(damaged, b'.');
-            let with_damaged = replace_first_unknown(damaged, b'#');
-            count_possibilities(with_operational.as_slice(), known)
-                + count_possibilities(with_damaged.as_slice(), known)
-        }
+    if segments.len() == 1 {
+        let count = count_segment_arrangements(&segments[0], lengths, 1);
+        // println!("returning {count}");
+        return count;
     }
+
+    let mut count = 0;
+
+    let first_segment = segments[0].as_slice();
+    if !first_segment.iter().any(|b| *b == b'#') {
+        count += count_arrangements(&segments[1..], lengths);
+    }
+
+    for taken_lengths in 1..=lengths.len() {
+        // println!("taken {taken_lengths}");
+        let arrangements = count_segment_arrangements(first_segment, &lengths[0..taken_lengths], 1);
+        count += arrangements * count_arrangements(&segments[1..], &lengths[taken_lengths..]);
+    }
+
+    count
 }
 
 fn main() {
     let file = File::open("input.txt").unwrap();
     let reader = BufReader::new(file);
-    let mut sum = 0;
-    for line in reader.lines().map(std::result::Result::unwrap) {
-        let mut split = line.split_whitespace();
-        let damaged = split.next().unwrap();
-        let known = Status::from_known(split.next().unwrap());
-        let possibilities = count_possibilities(damaged.as_bytes(), &known);
-        sum += possibilities;
-    }
+
+    let sum: usize = reader
+        .lines()
+        .map(std::result::Result::unwrap)
+        .map(|line| {
+            let mut split = line.split_whitespace();
+
+            let segments = split.next().unwrap();
+            let segments = (0..5).map(|_| segments).collect::<Vec<_>>();
+            let segments = segments.join("?");
+            let segments = segments
+                .split('.')
+                .filter_map(|segment| {
+                    if segment.is_empty() {
+                        None
+                    } else {
+                        Some(Vec::from(segment.as_bytes()))
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            let lengths = split
+                .next()
+                .unwrap()
+                .split(',')
+                .map(|length| length.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            let lengths = lengths
+                .iter()
+                .copied()
+                .cycle()
+                .take(5 * lengths.len())
+                .collect::<Vec<_>>();
+
+            println!(
+                "{:?} {lengths:?}",
+                segments
+                    .iter()
+                    .map(|segment| String::from_utf8(segment.clone()).unwrap())
+                    .collect::<Vec<_>>()
+            );
+
+            let arrangements = count_arrangements(&segments, &lengths);
+            // println!("{arrangements}");
+            arrangements
+        })
+        .sum();
+
     println!("{sum}");
 }
