774a189dd7faa54694155198a2e0f7f14f9b517a
Dan Stoza <dstoza@gmail.com>
2023-12-12 22:54:28 -0800

    2023-13: Add part 2 solution

--- 2023/day-13/src/main.rs
+++ 2023/day-13/src/main.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 fn transpose(rows: &[Vec<u8>]) -> Vec<Vec<u8>> {
-    let mut columns = vec![Vec::new(); rows[0].len()];
+    let mut columns = vec![Vec::with_capacity(rows.len()); rows[0].len()];
     for row in rows {
         for (column, byte) in row.iter().enumerate() {
             columns[column].push(*byte);
@@ -14,6 +14,7 @@ fn transpose(rows: &[Vec<u8>]) -> Vec<Vec<u8>> {
     columns
 }
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 enum Reflection {
     Row(usize),
     Column(usize),
@@ -59,22 +60,57 @@ fn reflection_score(reflections: &[Reflection]) -> usize {
         .sum()
 }
 
+fn smudge_at(rows: &mut [Vec<u8>], index: usize) {
+    let row = index / rows[0].len();
+    let column = index % rows[0].len();
+    rows[row][column] = match rows[row].get(column).unwrap() {
+        b'#' => b'.',
+        b'.' => b'#',
+        _ => unreachable!(),
+    }
+}
+
+fn find_alternate_reflection(mut rows: Vec<Vec<u8>>) -> Reflection {
+    let initial_reflections = find_all_reflections(&rows);
+    assert!(initial_reflections.len() == 1);
+    let initial_reflection = initial_reflections[0];
+
+    for smudge in 0..rows.len() * rows[0].len() {
+        if smudge > 0 {
+            smudge_at(&mut rows, smudge - 1);
+        }
+        smudge_at(&mut rows, smudge);
+
+        if let Some(alternate) = find_all_reflections(&rows)
+            .iter()
+            .find(|reflection| **reflection != initial_reflection)
+        {
+            return *alternate;
+        }
+    }
+
+    unreachable!()
+}
+
 fn main() {
     let file = File::open("input.txt").unwrap();
     let reader = BufReader::new(file);
 
     let mut rows = Vec::new();
     let mut total = 0;
+    let mut alternate_total = 0;
     for line in reader.lines().map(std::result::Result::unwrap) {
         if line.is_empty() {
             total += reflection_score(&find_all_reflections(&rows));
-            rows.clear();
+            alternate_total += reflection_score(&[find_alternate_reflection(rows)]);
+            rows = Vec::new();
             continue;
         }
 
         rows.push(Vec::from(line.as_bytes()));
     }
     total += reflection_score(&find_all_reflections(&rows));
+    alternate_total += reflection_score(&[find_alternate_reflection(rows)]);
 
-    println!("{total}");
+    println!("{total} {alternate_total}");
 }
