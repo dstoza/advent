29fc76b26471330a56cf26a2875b6b326a73beae
Dan Stoza <dstoza@gmail.com>
2023-12-25 21:27:25 -0800

    2023-24: Add part 1 solution

--- 2023/day-24/src/main.rs
+++ 2023/day-24/src/main.rs
@@ -5,7 +5,8 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-use nalgebra::{point, vector, Point3, Vector3};
+use approx::relative_eq;
+use nalgebra::{point, vector, Point2, Point3, Vector3};
 
 #[derive(Debug)]
 struct Ray {
@@ -32,6 +33,38 @@ impl Ray {
             ],
         }
     }
+
+    fn slope_intercept(&self) -> (f64, f64) {
+        let slope = self.direction.y / self.direction.x;
+        let intercept = self.origin.y - slope * self.origin.x;
+        (slope, intercept)
+    }
+
+    fn intersection_2d(&self, other: &Self) -> Option<Point2<f64>> {
+        // ax + b == cx + d
+        // ax - cx == d - b
+        // x == (d - b) / (a - c)
+        let (a, b) = self.slope_intercept();
+        let (c, d) = other.slope_intercept();
+
+        if relative_eq!(a, c) {
+            assert!(!relative_eq!(b, d));
+            return None;
+        }
+
+        let intersection_x = (d - b) / (a - c);
+        let intersection_y = a * intersection_x + b;
+        let intersection = point![intersection_x, intersection_y];
+
+        let self_time = (intersection_x - self.origin.x) / self.direction.x;
+        let other_time = (intersection_x - other.origin.x) / other.direction.x;
+
+        if self_time < 0.0 || other_time < 0.0 {
+            return None;
+        }
+
+        Some(intersection)
+    }
 }
 
 fn main() {
@@ -42,4 +75,21 @@ fn main() {
         .map(std::result::Result::unwrap)
         .map(|line| Ray::parse(&line))
         .collect::<Vec<_>>();
+
+    let intersection_range = 200_000_000_000_000.0..=400_000_000_000_000.0;
+
+    let mut in_range = 0;
+    for (index, ray) in rays.iter().enumerate() {
+        for other in &rays[index + 1..] {
+            if let Some(intersection) = ray.intersection_2d(other) {
+                if intersection_range.contains(&intersection.x)
+                    && intersection_range.contains(&intersection.y)
+                {
+                    in_range += 1;
+                }
+            }
+        }
+    }
+
+    println!("{in_range}");
 }
