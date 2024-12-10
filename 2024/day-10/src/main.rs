c85e7e333b98ea77b8492b9d27f4ec91f179bc37
Dan Stoza <dstoza@gmail.com>
2024-12-10 10:01:35 -0800

    2024-10: Remove unnecessary command line flag

--- 2024/day-10/src/main.rs
+++ 2024/day-10/src/main.rs
@@ -10,10 +10,6 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Args {
-    /// Part of the problem to run
-    #[arg(short, long, default_value_t = 1, value_parser = clap::value_parser!(u8).range(1..=2))]
-    part: u8,
-
     /// File to open
     filename: String,
 }
