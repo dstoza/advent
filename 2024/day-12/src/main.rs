5787db1b18309f658b18ec744e753a064310b349
Dan Stoza <dstoza@gmail.com>
2024-12-11 23:02:11 -0800

    2024-12: Add part 2 solution

--- 2024/day-12/src/main.rs
+++ 2024/day-12/src/main.rs
@@ -82,16 +82,79 @@ fn get_regions(mut grid: Vec<Vec<u8>>) -> Vec<HashSet<(usize, usize)>> {
     regions
 }
 
-fn region_perimeter(region: &HashSet<(usize, usize)>) -> usize {
-    region
-        .iter()
-        .map(|(row, column)| {
-            neighbors(*row, *column)
-                .iter()
-                .filter(|neighbor| !region.contains(*neighbor))
-                .count()
-        })
-        .sum()
+#[derive(Clone, Debug, Eq, PartialEq)]
+enum Side {
+    Top,
+    Right,
+    Bottom,
+    Left,
+}
+
+fn get_edges(region: &HashSet<(usize, usize)>) -> Vec<(Side, (usize, usize))> {
+    let mut edges = Vec::new();
+    for (row, column) in region {
+        if !region.contains(&(*row - 1, *column)) {
+            edges.push((Side::Top, (*row, *column)));
+        }
+        if !region.contains(&(*row, *column + 1)) {
+            edges.push((Side::Right, (*row, *column)));
+        }
+        if !region.contains(&(*row + 1, *column)) {
+            edges.push((Side::Bottom, (*row, *column)));
+        }
+        if !region.contains(&(*row, *column - 1)) {
+            edges.push((Side::Left, (*row, *column)));
+        }
+    }
+
+    edges
+}
+
+fn count_sides(mut edges: Vec<(Side, (usize, usize))>) -> usize {
+    let mut sides = 0;
+    while let Some((side, (row, column))) = edges.pop() {
+        let mut related = match side {
+            Side::Top | Side::Bottom => {
+                let mut related = vec![column];
+                edges.retain(|edge| {
+                    let (s, (r, c)) = edge;
+                    if *s != side || *r != row {
+                        return true;
+                    }
+
+                    related.push(*c);
+                    false
+                });
+
+                related
+            }
+            Side::Left | Side::Right => {
+                let mut related = vec![row];
+                edges.retain(|edge| {
+                    let (s, (r, c)) = edge;
+                    if *s != side || *c != column {
+                        return true;
+                    }
+
+                    related.push(*r);
+                    false
+                });
+
+                related
+            }
+        };
+
+        related.sort_unstable();
+
+        sides += 1;
+        for window in related.windows(2) {
+            if window[1] > window[0] + 1 {
+                sides += 1;
+            }
+        }
+    }
+
+    sides
 }
 
 fn main() {
@@ -100,13 +163,18 @@ fn main() {
     let file = File::open(args.filename).unwrap();
     let reader = BufReader::new(file);
 
-    println!("running part {}", args.part);
-
     let grid = parse_padded_grid(reader.lines().map(Result::unwrap));
     let regions = get_regions(grid);
     let mut price = 0;
     for region in regions {
-        price += region.len() * region_perimeter(&region);
+        let edges = get_edges(&region);
+        let sides = if args.part == 1 {
+            edges.len()
+        } else {
+            count_sides(edges)
+        };
+
+        price += region.len() * sides;
     }
 
     println!("{price}");
