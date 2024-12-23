66d50e789a4c778415d2ebf6cbca957bd70bab85
Dan Stoza <dstoza@gmail.com>
2024-12-22 23:00:56 -0800

    2024-23: Add part 2 solution

--- 2024/day-23/src/main.rs
+++ 2024/day-23/src/main.rs
@@ -10,12 +10,37 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Args {
-    /// Part of the problem to run
-    #[arg(short, long, default_value_t = 1, value_parser = clap::value_parser!(u8).range(1..=2))]
-    part: u8,
-
     /// File to open
     filename: String,
+}
+
+fn bron_kerbosch(
+    r: &HashSet<String>,
+    mut p: HashSet<String>,
+    mut x: HashSet<String>,
+    neighbors: &HashMap<String, HashSet<String>>,
+    maximal: &mut Vec<String>,
+) {
+    if p.is_empty() && x.is_empty() {
+        let mut m = r.iter().cloned().collect::<Vec<_>>();
+        if m.len() > maximal.len() {
+            m.sort_unstable();
+            *maximal = m;
+        }
+    }
+
+    let vertices = p.iter().cloned().collect::<Vec<_>>();
+    for v in &vertices {
+        bron_kerbosch(
+            &r.union(&HashSet::from([v.clone()])).cloned().collect(),
+            p.intersection(neighbors.get(v).unwrap()).cloned().collect(),
+            x.intersection(neighbors.get(v).unwrap()).cloned().collect(),
+            neighbors,
+            maximal,
+        );
+        p.remove(v);
+        x.insert(v.to_owned());
+    }
 }
 
 fn main() {
@@ -24,22 +49,20 @@ fn main() {
     let file = File::open(args.filename).unwrap();
     let reader = BufReader::new(file);
 
-    println!("running part {}", args.part);
-
-    let mut connections = HashMap::new();
+    let mut neighbors = HashMap::new();
     for line in reader.lines().map(Result::unwrap) {
         let mut split = line.split('-');
         let a = split.next().unwrap().to_owned();
         let b = split.next().unwrap().to_owned();
 
-        connections
+        neighbors
             .entry(a.clone())
             .and_modify(|others: &mut HashSet<_>| {
                 others.insert(b.clone());
             })
             .or_insert_with(|| HashSet::from([b.clone()]));
 
-        connections
+        neighbors
             .entry(b)
             .and_modify(|others: &mut HashSet<_>| {
                 others.insert(a.clone());
@@ -48,12 +71,12 @@ fn main() {
     }
 
     let mut trios = HashSet::new();
-    for (from, to) in connections
+    for (from, to) in neighbors
         .iter()
         .flat_map(|(from, to)| to.iter().map(|t| (from.to_owned(), t)))
     {
-        let from_connections = connections.get(&from).unwrap();
-        let to_connections = connections.get(to).unwrap();
+        let from_connections = neighbors.get(&from).unwrap();
+        let to_connections = neighbors.get(to).unwrap();
         for third in from_connections.intersection(to_connections) {
             let mut trio = [from.clone(), to.to_owned(), third.to_owned()];
             trio.sort_unstable();
@@ -66,6 +89,17 @@ fn main() {
         .filter(|trio| trio.iter().any(|computer| computer.starts_with('t')))
         .count();
 
-    // 2398 too high
     println!("{count}");
+
+    let vertices = neighbors.keys().cloned().collect::<HashSet<_>>();
+    let mut maximal = Vec::new();
+    bron_kerbosch(
+        &HashSet::new(),
+        vertices,
+        HashSet::new(),
+        &neighbors,
+        &mut maximal,
+    );
+
+    println!("{}", maximal.join(","));
 }
