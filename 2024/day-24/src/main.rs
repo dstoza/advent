2b36c7be9f0721a33a27d8d5481ab5a5b26ac647
Dan Stoza <dstoza@gmail.com>
2024-12-24 20:57:08 -0800

    2024-24: Add automatic solver for part 2

--- 2024/day-24/src/main.rs
+++ 2024/day-24/src/main.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::pedantic)]
 
 use std::{
+    cmp::Ordering,
     collections::{HashMap, VecDeque},
     fs::File,
     io::{BufRead, BufReader},
@@ -10,15 +11,11 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Args {
-    /// Part of the problem to run
-    #[arg(short, long, default_value_t = 1, value_parser = clap::value_parser!(u8).range(1..=2))]
-    part: u8,
-
     /// File to open
     filename: String,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 enum Op {
     And,
     Or,
@@ -36,7 +33,7 @@ impl Op {
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 struct Gate {
     a: String,
     op: Op,
@@ -45,13 +42,21 @@ struct Gate {
 }
 
 impl Gate {
+    fn new(mut a: String, op: Op, mut b: String, out: String) -> Self {
+        if a.cmp(&b) == Ordering::Greater {
+            std::mem::swap(&mut a, &mut b);
+        }
+        Self { a, op, b, out }
+    }
+
+    #[allow(clippy::needless_pass_by_value, reason = "Allows easier map")]
     fn parse(line: String) -> Self {
         let mut split = line.split_whitespace();
         let a = split.next().unwrap().to_owned();
         let op = Op::from_str(split.next().unwrap());
         let b = split.next().unwrap().to_owned();
         let out = split.nth(1).unwrap().to_owned();
-        Self { a, op, b, out }
+        Self::new(a, op, b, out)
     }
 
     fn resolve(&self, gates: &HashMap<String, bool>) -> Option<bool> {
@@ -61,6 +66,122 @@ impl Gate {
             Op::And => Some(*a && *b),
             Op::Or => Some(*a || *b),
             Op::Xor => Some(*a ^ *b),
+        }
+    }
+
+    fn almost_match(&self, other: &Self) -> Option<(String, String)> {
+        if self.op != other.op {
+            return None;
+        }
+
+        let score = u8::from(self.a == other.a)
+            + u8::from(self.b == other.b)
+            + u8::from(self.out == other.out);
+
+        if score != 2 {
+            return None;
+        }
+
+        if self.a != other.a {
+            return Some((self.a.clone(), other.a.clone()));
+        }
+
+        if self.b != other.b {
+            return Some((self.b.clone(), other.b.clone()));
+        }
+
+        if self.out != other.out {
+            return Some((self.out.clone(), other.out.clone()));
+        }
+
+        unreachable!()
+    }
+}
+
+fn get_output(gates: &[Gate], term: &Gate) -> String {
+    gates
+        .iter()
+        .find_map(|gate| {
+            if let Some((output, _)) = gate.almost_match(term) {
+                return Some(output);
+            }
+
+            None
+        })
+        .unwrap()
+}
+
+fn find_flip(gates: &[Gate]) -> Option<(String, String)> {
+    let carry_term = Gate::new(
+        String::from("x00"),
+        Op::And,
+        String::from("y00"),
+        String::new(),
+    );
+    let mut carry = gates
+        .iter()
+        .find_map(|gate| {
+            if let Some((carry, _)) = gate.almost_match(&carry_term) {
+                return Some(carry);
+            }
+
+            None
+        })
+        .unwrap();
+
+    let mut bit = 1;
+    loop {
+        let x = format!("x{bit:02}");
+        let y = format!("y{bit:02}");
+
+        if !gates
+            .iter()
+            .any(|gate| gate.a == x && gate.op == Op::Xor && gate.b == y)
+        {
+            return None;
+        }
+
+        let sum = get_output(
+            gates,
+            &Gate::new(x.clone(), Op::Xor, y.clone(), String::new()),
+        );
+
+        let generate = get_output(gates, &Gate::new(x, Op::And, y, String::new()));
+
+        let z = format!("z{bit:02}");
+        let out = Gate::new(carry.clone(), Op::Xor, sum.clone(), z);
+        if !gates.iter().any(|gate| *gate == out) {
+            return Some(
+                gates
+                    .iter()
+                    .find_map(|gate| gate.almost_match(&out))
+                    .unwrap(),
+            );
+        }
+
+        let propagate = get_output(gates, &Gate::new(carry, Op::And, sum, String::new()));
+        carry = get_output(
+            gates,
+            &Gate::new(generate, Op::Or, propagate, String::new()),
+        );
+
+        bit += 1;
+    }
+}
+
+fn apply_flip(gates: &mut [Gate], flip: (String, String)) {
+    let (a, b) = flip;
+    for gate in &mut *gates {
+        if gate.out == a {
+            gate.out = String::from("replace");
+        }
+        if gate.out == b {
+            gate.out.clone_from(&a);
+        }
+    }
+    for gate in gates {
+        if gate.out == "replace" {
+            gate.out.clone_from(&b);
         }
     }
 }
@@ -87,6 +208,7 @@ fn main() {
     }
 
     let mut gates = lines.map(Gate::parse).collect::<VecDeque<_>>();
+    let mut gate_list = gates.iter().cloned().collect::<Vec<_>>();
     while let Some(gate) = gates.pop_front() {
         if let Some(result) = gate.resolve(&wires) {
             wires.insert(gate.out, result);
@@ -107,4 +229,14 @@ fn main() {
     }
 
     println!("{result}");
+
+    let mut flips = Vec::new();
+    while let Some(flip) = find_flip(&gate_list) {
+        flips.push(flip.0.clone());
+        flips.push(flip.1.clone());
+        apply_flip(&mut gate_list, flip);
+    }
+
+    flips.sort_unstable();
+    println!("{}", flips.join(","));
 }
